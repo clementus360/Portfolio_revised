1291f21d8c05435f7b4441e348b2ecde31e78eea
clementus360 <clementus360@gmail.com>
2023-03-17 15:32:06 +0200

    audit

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/tailwind.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -69,5 +69,8 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['gsap'],
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
   },
 }
